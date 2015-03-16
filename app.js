ca7cab0b295f45535282ee944f5a0b8199fd3f25
MIJOTHY <james.griffiths.2222@googlemail.com>
2015-03-16 16:54:45 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
 var server = require("./api/server.js");
 
-server.connection({
-	host: "localhost",
-	port: process.env.PORT || 8080
-});
-
-
 server.start(function() {
 	console.log("Server running at " + server.info.uri);
 });
