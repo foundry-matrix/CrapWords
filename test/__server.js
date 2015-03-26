d81afc62bf63ac515741c07eb4d0140b0f4d2433
gregaubs <gaj.aubert@gmail.com>
2015-03-26 12:04:20 +0000

    empty tests written

--- test/__server.js
+++ test/__server.js
@@ -4,21 +4,51 @@ var server 	= require("../api/server.js");
 var handler = require('../handler.js');
 
 
-// TEST 1
-lab.experiment("A basic server test: ", function() {
 
- 	var options = {
- 		url: "/",
- 		method: "GET"
- 	};
- 	lab.test("The home page ", function(done) {
 
- 		server.inject(options, function(response) {
- 			//console.log(response);
- 			assert.equal(response.statusCode, 200, "should return a 200 status code");
- 			assert.equal(typeof response.result, "string", "should reply with a string");
- 			done();
- 		});
- 	});
- });
+//EMPTY TESTS
+
+lab.experiment("When the user clicks the SUBMIT button", function() {
+
+	lab.test("if new, the user's email and search data object is saved to db", function(done) {
+
+	});
+
+
+	lab.test("if existing, a search object is appended to the document in the db", function(done) {
+
+	});
+
+
+	lab.test("the user's mongoDB ID is extracted from the db", function(done) {
+
+	});
+
+
+	lab.test("a unique URL based taking the form: '/' + ID  is stored as a var ", function(done) {
+
+	});
+
+
+	lab.test("the Webshot module is invoked and pointed to the unique URL", function(done) {
+
+	});
+
+
+	lab.test("routing logic handles the http request for the unique URL and serves correct html, css and js file", function(done) {
+
+	});
+
+
+	lab.test("the JS file immediately invokes an AJAX request for data from the db based on the user's ID", function(done) {
+
+	});
+
+
+	lab.test("the screenshot is set as an attachement and Mandrill sends an email to the user", function(done) {
+
+	});
+
+	
+});
 
